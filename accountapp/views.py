f48fb384e1fbda9aa3960ef3766618df7c0f9551
GyuHwan Jung <foxrain87@naver.com>
2021-12-24 16:49:34 +0900

    django course 18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -14,7 +16,11 @@ def hello_world(request):
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+
+
+        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
+        hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
 
